04d3cd364769d8041bb24d953bdd137b35cac3f9
Joshua-Moe <59875735+Joshua-Moe@users.noreply.github.com>
2024-04-18 13:31:13 -0400

    Add files via upload
    First commit

---
+++ PyBank/main.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import argparse
+parser = argparse.ArgumentParser(description='Helpful description here')
+parser.add_argument('--file', type=str, help="Input file for PyBank", required=True)
+args = parser.parse_args()
+
+# One-liner to copy/paste into command line:
+# python3 main.py --file="./Resources/budget_data.csv"
+
+""" Module 3 Challenge
+
+PyBank Instructions
+In this Challenge, you are tasked with creating a Python script to analyze the financial records of your company. 
+You will be given a financial dataset called budget_data.csv. The dataset is composed of two columns: "Date" and "Profit/Losses".
+
+Your task is to create a Python script that analyzes the records to calculate each of the following values:
+
+The total number of months included in the dataset
+
+The net total amount of "Profit/Losses" over the entire period
+
+The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+The greatest increase in profits (date and amount) over the entire period
+
+The greatest decrease in profits (date and amount) over the entire period
+
+
+"""
+
+def PyBank(file):
+
+    summary_statement  = "Financial Analysis" + "\n\n" + "----------------------------\n\n"
+    with open(file, 'r') as fh:
+        date = []
+        profit_losses = []
+        lines = fh.readlines()
+        header = lines[0] # I don't need the header for this; for the grading ruberic.
+        for line in lines[1:]:
+            date.append(line.strip().split(",")[0])
+            profit_losses.append(float(line.strip().split(",")[1]))
+        
+        # The total number of months included in the dataset
+        total_months = len(set(date))
+        
+        # The net total amount of "Profit/Losses" over the entire period
+        net_total = sum(profit_losses)
+        
+        # The changes in "Profit/Losses" over the entire period, and then the average of those changes
+        delta_profit = []
+        for i in range(len(profit_losses)-1):
+            delta_profit.append(float(profit_losses[i+1]-profit_losses[i]))
+            
+        averageChanges = round(sum(delta_profit)/len(delta_profit), 2)
+        
+        
+        #The greatest increase in profits (date and amount) over the entire period
+        # The greatest decrease in profits (date and amount) over the entire period
+        
+        for i in range(len(delta_profit)):
+            if delta_profit[i] == max(delta_profit):
+                greatestIncrease_date = date[i+1]
+            elif delta_profit[i] == min(delta_profit):
+                greatestDecrease_date = date[i+1]
+            else:
+                continue
+        
+        
+        summary_statement += f"Total Months: {total_months}\n\n"
+        summary_statement += f"Total: ${net_total}\n\n"
+        summary_statement += f"Average Change: ${averageChanges}\n\n"
+        summary_statement += f"Greatest Increase in Profits: {greatestIncrease_date} (${max(delta_profit)})\n\n"
+        summary_statement += f"Greatest Decrease in Profits: {greatestDecrease_date} (${min(delta_profit)})"
+        print(summary_statement)
+            
+    # Saving to output file
+    with open("./analysis/PyBank_output.txt",'w') as fh:
+        fh.write(summary_statement)
+        
+if __name__=="__main__":
+    PyBank(args.file)
---
+++ PyPoll/main.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import argparse
+parser = argparse.ArgumentParser(description='Helpful description here')
+parser.add_argument('--file', type=str, help="Input file for PyBank", required=True)
+args = parser.parse_args()
+
+# One-liner to copy/paste into command line:
+# python3 main.py --file="./Resources/election_data.csv"
+
+""" Module 3 Challenge
+
+PyPoll Instructions
+In this Challenge, you are tasked with helping a small, rural town modernize its vote-counting process.
+
+You will be given a set of poll data called election_data.csv. The dataset is composed of three columns: "Voter ID", "County", and "Candidate". 
+Your task is to create a Python script that analyzes the votes and calculates each of the following values:
+
+The total number of votes cast
+
+A complete list of candidates who received votes
+
+The percentage of votes each candidate won
+
+The total number of votes each candidate won
+
+The winner of the election based on popular vote
+
+
+"""
+
+def PyPoll(file):
+    election_dict = {}
+    with open(file, 'r') as fh:
+        lines = fh.readlines()
+        header = lines[0] # I don't need the header for this; for the grading ruberic.
+        for line in lines[1:]:
+            ballot_ID, County, Candidate = line.strip().split(",")
+            election_dict[Candidate] = election_dict.get(Candidate, 0) + 1
+        
+        # The total votes cast
+        totalVotes = sum(list(election_dict.values()))
+        
+        # A complete list of candidates who received votes
+        candidate_list = list(election_dict.keys())
+        # print(len(candidate_list)) --> There are only 3 candidates
+        
+        # The percentage of votes each candidate won
+        vote_stat_dict = {}
+        for key,value in election_dict.items():
+            vote_stat_dict[key] = round((value/totalVotes)*100, 3)
+        
+        # The total number of votes each candidate won
+        tuple_list = []
+        for key,value in election_dict.items():
+            tuple_list.append((value, key))
+        tuple_list.sort(reverse=True)
+        
+        # The winner of the election based on popular vote
+        winner = tuple_list[0][1]
+        
+        
+        results = "Election Results\n\n" + "-------------------------\n\n"
+        results += f"Total Votes: {totalVotes}\n\n"
+        results += "-------------------------\n\n"
+        results += f"{candidate_list[0]}: {vote_stat_dict[candidate_list[0]]}% ({election_dict.get(candidate_list[0])})\n\n"
+        results += f"{candidate_list[1]}: {vote_stat_dict[candidate_list[1]]}% ({election_dict.get(candidate_list[1])})\n\n"
+        results += f"{candidate_list[2]}: {vote_stat_dict[candidate_list[2]]}% ({election_dict.get(candidate_list[2])})\n\n"
+        results += "-------------------------\n\n"
+        results += f"Winner: {winner}\n\n"
+        results += "-------------------------\n\n"
+        print(results)
+    
+    # Saving to output file
+    with open("./analysis/PyPoll_output.txt",'w') as fh:
+        fh.write(results)
+    
+    
+if __name__=="__main__":
+    PyPoll(args.file)
